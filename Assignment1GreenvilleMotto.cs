a2f4d157e440e8ef47031d5299abef00d5709f68
Alley Hernandez <55570090+alleyhernandez@users.noreply.github.com>
2021-02-18 18:16:12 -0800

    Create Assignment1GreenvilleMotto.cs
    practicing printing in C#

---
+++ Assignment1GreenvilleMotto.cs
@@ -0,0 +1,9 @@
+class GreenvilleMotto
+{
+    static void Main()
+    {
+        System.Console.WriteLine("********************************************");
+        System.Console.WriteLine("*******The stars shine in Greenville.*******");
+        System.Console.WriteLine("********************************************");
+    }
+}
